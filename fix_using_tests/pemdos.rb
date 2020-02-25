762867038c896e91718718e12bdf508eb940884a
Aidan K McBride <aidankmcbride@gmail.com>
2020-02-25 21:14:03 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,13 @@
+require 'pry'
 def snake_it_up(string)
-  if string[0] == "s"
-  10 * "s" + string
-  else
-  string
-  end
+   if string[0] == "s"
+  i = 0
+    while i < 10 do
+      string.prepend("s")
+      i += 1
+    end
+    string
+  else	  
+  string	    
+  end	  
 end
